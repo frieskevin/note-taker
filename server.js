e61e0e99db60f0d619c199a21d809714ee5f932a
alotofsnakes <mrfries@gmail.com>
2022-09-28 17:34:47 -0700

    finished pathing and added comments

--- server.js
+++ server.js
@@ -1,29 +1,49 @@
-
+//requirement declarations
 const express = require('express');
-const notes = require('./Develop/db/db.json');
+const { notes } = require('./Develop/db/db.json');
 const fs = require('fs');
 const path = require('path');
 
+//heroku port or port 3001
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('./develop/public'));
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
-});
+//function to write new notes to notes array json
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, '/develop/db/db.json'),
+        JSON.stringify({notes: notesArray}, null, 2)
+    );
+    return note;
+}
 
+//gets the notes html page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/develop/public/notes.html'));
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/develop/public/index.html'));
+//gets the notes api data
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
 });
 
+//adds to the notes api data
 app.post('/api/notes', (req, res) => {
     req.body.id = notes.length.toString();
+    const note = createNewNote(req.body, notes)
+    res.json(note);
+});
+
+//wildcard request that just returns anything that hasn't been declared a path yet to index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/develop/public/index.html'));
 });
 
 app.listen(PORT, () => {
